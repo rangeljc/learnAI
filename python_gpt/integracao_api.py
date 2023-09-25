c753f2013ae2c2fe39b81524d93d2c6a6e4a8543
Julio Cesar Rangel <rangeljc@gmail.com>
2023-09-25 15:42:23 -0300

    Iniciando treinamento sobre integração de Python com GPT

---
+++ python_gpt/integracao_api.py
@@ -0,0 +1,34 @@
+import openai
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+prompt_sistema = '''
+Você é um categorizador de produtos.
+Voce deve
+'''
+
+resposta = openai.ChatCompletion.create(
+    model = 'gpt-3.5-turbo',
+    messages = [
+        {
+            'role': 'system',
+            'content': prompt_sistema
+        },
+        {
+            'role': 'user',
+            'content': 'Gere 5 produtos'
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+#print(resposta)
+print(resposta.choices[0].message.content)
